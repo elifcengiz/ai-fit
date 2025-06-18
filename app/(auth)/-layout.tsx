0fe980ad4b4ce4621c703f82a26ed8bcf1115654
elifcengiz <129393793+elifcengiz@users.noreply.github.com>
2025-06-18 15:11:58 +0300

    Update -layout.tsx

--- app/(auth)/-layout.tsx
+++ app/(auth)/-layout.tsx
@@ -1,1 +1,15 @@
+import { Stack } from 'expo-router';
+import { View } from 'react-native';
 
+export default function AuthLayout() {
+  return (
+    <Stack 
+      screenOptions={{ 
+        headerShown: false,
+        contentStyle: { backgroundColor: '#FFFFFF' }
+      }}
+    >
+      <Stack.Screen name="login" />
+    </Stack>
+  );
+}
